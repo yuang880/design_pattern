116e52d6874d9e96139934d707e31294718a9a09
zhangxd <yuang50915@gmail.com>
2023-04-23 21:24:31 +0800

    5-提交

---
+++ design-15-0/src/main/java/com/zhangxd/demo/design/group/GroupStructure.java
@@ -0,0 +1,7 @@
+package com.zhangxd.demo.design.group;/**
+ * 
+ * @author zhangxd
+ * @version 1.0 2023/4/15
+ */
+public class GroupStructure {
+}
---
+++ design-15-0/src/main/java/com/zhangxd/demo/design/lang/Collection.java
@@ -0,0 +1,7 @@
+package com.zhangxd.demo.design.lang;/**
+ * 
+ * @author zhangxd
+ * @version 1.0 2023/4/15
+ */
+public interface Collection {
+}
